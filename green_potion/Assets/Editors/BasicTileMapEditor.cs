5cbfbfccf3af728e9c0c3497a8a68944c4678a88
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-10-19 02:00:49 +0100

    Tested out some editor scripting

---
+++ green_potion/Assets/Editors/BasicTileMapEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+[CustomEditor(typeof(Tilemap))]
+public class BasicTileMapEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		/*Tilemap theTarget = (Tilemap)target;
+		theTarget.animationFrameRate = EditorGUILayout.FloatField("Animation Frame Rate: ", theTarget.animationFrameRate);*/
+		DrawDefaultInspector();
+	}
+}
